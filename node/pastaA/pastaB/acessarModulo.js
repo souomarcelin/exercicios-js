801dded3941808d20aeaba29009c9800b7bd3b50
Marcelo Chaves <9marceli@gmail.com>
2022-05-06 18:33:29 -0300

    Continuação do módulo de node

--- node/pastaA/pastaB/acessarModulo.js
+++ node/pastaA/pastaB/acessarModulo.js
@@ -1,2 +1,13 @@
 const moduloA = require('../../moduloA')
 console.log(moduloA.ola)
+
+const c = require('./pastaC')
+
+console.log(c.ola2)
+
+// const http = require('http')
+// const { Console } = require('console')
+// http.createServer((req,res) =>{
+//     res.write('Bom dia!')
+//     res.end()
+// }), listen(9090)
